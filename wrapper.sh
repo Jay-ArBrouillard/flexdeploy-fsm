b4e0032b2a2f0e85f8fb67e62834b4142e0d4816
jayar <jay-ar.brouillard@flexagon.com>
2024-02-01 12:30:54 -0600

    Commited by https://fdtlt83.flexagon.azure.com:8443 - CSV Download Advanced Setup Task To Git - 1.0.2

--- wrapper.sh
+++ wrapper.sh
@@ -8,7 +8,7 @@ setOutput()
         echo "Key and value required on setOutput function call"
         exit 1
     fi
-    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408085/2887985/internal/outputs/${1}"
+    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408226/2888040/internal/outputs/${1}"
 }
 
 

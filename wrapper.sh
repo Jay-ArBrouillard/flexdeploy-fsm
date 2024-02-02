91caf79d2fca6a953d04fb819469c15b6a6e0ee3
releaseeventprocessor <jay-ar.brouillard@flexagon.com>
2024-02-02 02:46:08 -0600

    Commited by https://fdtlt83.flexagon.azure.com:8443 - CSV Download Setup Task Push to Git - 7.0.0.3.233

--- wrapper.sh
+++ wrapper.sh
@@ -8,7 +8,7 @@ setOutput()
         echo "Key and value required on setOutput function call"
         exit 1
     fi
-    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408085/2894979/internal/outputs/${1}"
+    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408085/2895027/internal/outputs/${1}"
 }
 
 
@@ -16,4 +16,4 @@ git stash
 git pull origin main
 git stash pop
 git add .
-git -c user.name="$FD_WF_USER" -c user.email="$FD_WF_USER_EMAIL" commit -m "Commited by $FD_SERVER_BASE_URL  -  $FD_PROJECT_NAME -  $FD_PROJECT_VERSION"
+git -c user.name="$FD_WF_USER" -c user.email="$FD_WF_USER_EMAIL" commit -m "Commited by $FD_SERVER_BASE_URL - $FD_PROJECT_NAME - $FD_PROJECT_VERSION"

c39050a609f1274cafe14c8ba06e120e3d655439
releaseeventprocessor <jay-ar.brouillard@flexagon.com>
2024-02-02 10:16:23 -0600

    Commited by https://fdtlt83.flexagon.azure.com:8443 - CSV Download Setup Task Push to Git - 7.0.0.3.233

--- wrapper.sh
+++ wrapper.sh
@@ -8,7 +8,11 @@ setOutput()
         echo "Key and value required on setOutput function call"
         exit 1
     fi
+<<<<<<< Updated upstream
     echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408085/2898113/internal/outputs/${1}"
+=======
+    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408085/2898118/internal/outputs/${1}"
+>>>>>>> Stashed changes
 }
 
 

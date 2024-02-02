3e86f335bf58aff681c70eb0aae7ddbeabf155b7
jayar <jay-ar.brouillard@flexagon.com>
2024-02-02 11:09:42 -0600

    Commited by https://fdtlt83.flexagon.azure.com:8443 - CSV Download Advanced Setup Task To Git - 1.0.24

--- wrapper.sh
+++ wrapper.sh
@@ -8,20 +8,8 @@ setOutput()
         echo "Key and value required on setOutput function call"
         exit 1
     fi
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408085/2898113/internal/outputs/${1}"
-=======
-    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408085/2898118/internal/outputs/${1}"
->>>>>>> Stashed changes
-=======
-    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408085/2898115/internal/outputs/${1}"
->>>>>>> Stashed changes
+    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408226/2899585/internal/outputs/${1}"
 }
 
 
-git stash
 git pull origin main
-git stash pop
-git add .
-git -c user.name="$FD_WF_USER" -c user.email="$FD_WF_USER_EMAIL" commit -m "Commited by $FD_SERVER_BASE_URL - $FD_PROJECT_NAME - $FD_PROJECT_VERSION"

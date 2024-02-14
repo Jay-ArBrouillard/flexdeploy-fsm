7f148f01c66ee176fec14c161711cd7bc8c6c27f
muthu <muthu.sankaralingam@flexagon.com>
2024-02-14 09:24:56 -0600

    Commited by https://fdtlt83.flexagon.azure.com:8443 - CSV Download Advanced Setup Task To Git - 1.0.59

--- wrapper.sh
+++ wrapper.sh
@@ -8,8 +8,15 @@ setOutput()
         echo "Key and value required on setOutput function call"
         exit 1
     fi
-    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408085/2991739/internal/outputs/${1}"
+    echo "${2}" > "/u01/flexdeploy/application/localhost/work/11408226/2992487/internal/outputs/${1}"
 }
 
 
 git pull origin main
+git add SetupTasks/*
+if [ -n "$(git status --porcelain)" ]; then
+echo "there are changes";
+setOutput CHANGES true
+else
+echo "no changes";
+fi
